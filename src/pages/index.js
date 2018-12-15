77aa90677332ceeb5141d3039766c6b0fe7fe261
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-16 00:32:52 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,11 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
+import pic01 from '../assets/images/BAHIAZUL-VILLAS-3.jpg'
+import pic02 from '../assets/images/Babylonstoren-Hotel-Winery-Vineyard-Franschhoek-1.jpg'
+import pic03 from '../assets/images/Hotel-Alaia-Punta-de-Lobos-6-1.jpg'
+import pic04 from '../assets/images/Hudhuranfushi-4.jpg'
+import pic05 from '../assets/images/Kimpton-Shorebreak-Hotel-3.jpg'
 import pic06 from '../assets/images/pic06.jpg'
 
 class HomeIndex extends React.Component {
