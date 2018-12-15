461e2cdd065f54875c52b02b3752c165594e6e9d
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-16 00:46:35 +0100

    Update landing.js

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -4,9 +4,9 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerLanding from '../components/BannerLanding'
 
-import pic08 from '../assets/images/pic08.jpg'
-import pic09 from '../assets/images/pic09.jpg'
-import pic10 from '../assets/images/pic10.jpg'
+import pic08 from '../assets/images/Babylonstoren-Hotel-Winery-Vineyard-Franschhoek-6.jpg'
+import pic09 from '../assets/images/Babylonstoren-Hotel-Winery-Vineyard-Franschhoek-7.jpg'
+import pic10 from '../assets/images/Babylonstoren-Hotel-Winery-Vineyard-Franschhoek-8.jpg'
 
 const Landing = (props) => (
     <Layout>
