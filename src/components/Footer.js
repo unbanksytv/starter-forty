e15bf5ed119970b6de14820094b04297de5f87db
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-16 00:15:26 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ const Footer = (props) => (
         <div className="inner">
             <ul className="icons">
                 <li><a href="https://twitter.com/livethelifetv" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a href="https://www.facebook.com/LiveTheLife/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="https://www.instagram.com/livethelife.tv/" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://github.com/yvesvandenmeerssche" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/in/yvesvandenmeerssche/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
             <ul className="copyright">
                 <li>&copy; On Ne Lache Rien</li><li>Design: <a href="https://www.livethelife.tv">LTL</a></li>
