4bbe965ac41d51a09636f11c19735de10f9226b3
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-16 00:44:42 +0100

    Update generic.js

--- src/pages/generic.js
+++ src/pages/generic.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-import pic11 from '../assets/images/pic11.jpg'
+import pic11 from '../assets/images/17620485_641438259386466_713045837703015075_o.jpg'
 
 const Generic = (props) => (
     <Layout>
