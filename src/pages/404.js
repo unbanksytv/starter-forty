820d579eacae76cc24b2a0fc2cc4cd0fc82d369c
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-15 23:42:08 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
     <div id="main" className="alt">
         <section id="one">
             <div className="inner">
-                <h1>NOT FOUND</h1>
+                <h1>LOST IN TRANSLATION</h1>
                 <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
             </div>
         </section>
