dbe2e86da2ffaf00be0c326bb54bcd57c31faa1f
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-16 08:33:19 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#03182b',
+        theme_color: '#03182b',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
