59a86d5796a8dc94454ccaf049e6ff8e927a4277
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-16 14:22:31 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>One Ne Lache Rien. #ONLR</h1>
+                <h1>On Ne Lache Rien. #ONLR</h1>
             </header>
             <div className="content">
                 <p>JAM stands for Javascript API & Markup.<br />
